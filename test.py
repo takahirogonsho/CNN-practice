ebf1126cdc88185aa5e3e952b43caab6bc93eb4f
takahirogonsho <42526082+takahirogonsho@users.noreply.github.com>
2018-10-31 12:10:35 +0900

    Revert "test.pyの変更"

--- test.py
+++ test.py
@@ -1,14 +1,1 @@
-#画像の読み込み
-
-images = []
-path = 'C:/Users/koji_yamamoto/Desktop/自習/hiragana73/U304A/'
-for i in os.listdir(path):
-    images.append(path + str(i))
-    
-
-plt.figure(figsize = (20,20))
-for i, file in enumerate(images[:20]):
-    img = Image.open(file)
-    im = np.array(img)
-    plt.subplot(5,4, i + 1)
-    plt.imshow(im)
+test
